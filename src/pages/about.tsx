db42c2f9d8148a75d0898824fe187fa5679f5f9c
ChachyDev <shalomadecoolboy@outlook.com>
2022-08-03 20:33:15 +0100

    feat: add about us test

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,186 @@
 const About = () => (
-    <div className="m-6">
-        <h1 className="text-[#1E88E5] text-[42px] md:text-[57px] text-bold">About us</h1>
-        <p className="text-[#666666] md:text-[32px] text-[20px] text-bold">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-        </p>
-    </div>
-);
+        <div className="m-6">
+            <h1 className="text-[#1E88E5] text-[42px] md:text-[57px] text-bold">About us</h1>
+            <div className="text-[#666666] md:text-[20px] text-[15px] text-bold">
+                <p>
+                    In July 1909, a son was born into the Akindayomi family of Ondo State of Nigeria. Even though this child
+                    grew up surrounded by idol worshippers, he knew there existed a greater power and yearned to know,
+
+                    “The God who created the earth and everyone on it”. This pursuit for God led him to the Church
+                    Missionary
+                    Society where he was baptized in 1927. Still spiritually unfulfilled, he joined the Cherubim and
+                    Seraphim
+                    church in 1931.
+
+                    Whilst there, he began to hear a voice within him saying, “You will be my servant.” Since this was not
+                    his
+                    intention, he decided to ignore the voice. This went on for seven years during which all the business
+                    ventures that he tried resulted into failure. In debt and without peace of mind, he found himself
+                    totally
+                    dependent on the grace of God. Here marked the beginning of a definite relationship with God.Totally
+                    broken,
+                    he yielded saying, “Lord, I will go wherever you want me to go.” He asked for signs to confirm that this
+                    was
+                    indeed God’s call. The confirmation came through the Bible passages of Jeremiah 1:4-10, Isaiah 41:10-13
+                    and
+                    Romans 8:29-31. The Lord assured him that He would provide for all his needs, as he would not receive
+                    any
+                    salary from that point on. This proved to be a comforting reminder during the trials in the months
+                    ahead. He
+                    became married in 1941. He continued to worship with the Cherubim and Seraphim. In 1947, he started to
+                    become concerned that the church was departing from the true Word of God in some of its practices. By
+                    1952,
+                    he felt totally persuaded to leave the church. He started at Willoughby Street, Ebute-Metta, Lagos a
+                    housefellowship called, the Glory of God Fellowship. Initially there were nine members but before long
+                    the
+                    fellowship rapidly grew as the news of the miracles that occurred in their midst spread.
+
+                    Akindayomi also had a vision of words that appeared to be written on a blackboard. The words were “The
+                    Redeemed Christian Church of God.” Amazingly, Pa Akindayomi who could not read or write was
+                    supernaturally
+                    able to write these words down. In this visitation, God also said to him that this church would go to
+                    the
+                    ends of the earth and that when the Lord Jesus Christ appeared in glory, He would meet the church. The
+                    Lord
+                    then established a covenant with Pa Akindayomi, synonymous to the Abrahamic covenant in the Bible. He
+                    said
+                    that He the Lord would meet all the needs of the church in an awesome way if only members would serve
+                    Him
+                    faithfully and be obedient to His Word. It is upon this covenant that the Redeemed Christian Church of
+                    God
+                    was built.
+
+                    Thus, the Redeemed Christian Church of God was born in 1952, destined by the Lord Himself to take the
+                    world
+                    for Him. The church continued to meet at 9 Willoughby Street until they were able to acquire some land
+                    thereby witnessing a relocation to the present site of the Headquarters of the church at 1-5 Redemption
+                    Way,
+                    Ebute-Metta, Lagos (formerly 1a, Cemetery Street).
+
+                    Sometime in the early 70s, God had spoken to Pa Akindayomi about his successor. The Lord told him that
+                    this
+                    man who was not a member of the church then, would be a young educated man. Thus when a young university
+                    lecturer joined the church in 1973, Papa was able to recognize him in the Spirit as the one that the
+                    Lord
+                    had spoken about in the past. This man, Enoch Adejare Adeboye who was then a lecturer of Mathematics at
+                    the
+                    University of Lagos soon became involved in the church. He became one of the interpreters translating Pa
+                    Akindayomi’s sermons from Yoruba to English. He was ordained a pastor of the church in 1975.
+
+                    Papa was preparing to meet his Creator. He sent for Pastor Adeboye and spent several hours sharing with
+                    him
+                    details of the covenant and the plans of the Lord for the church. Even though a year before this, the
+                    Lord
+                    had revealed to Pastor Adeboye that he would be Papa’s successor, it was still too difficult for him to
+                    fully contemplate such an awesome responsibility.
+
+                    Pa Josiah Akindayomi was 71 years old when he died. Amidst controversy, Pastor Adeboye’s appointment was
+                    formalized by the reading of Pa Akindayomi’s sealed pronouncement after his burial.
+
+                    Since 1981, an open explosion began with the number of parishes growing in leaps and bounds. At the last
+                    count, there are at least about 2000 parishes of the Redeemed Christian Church of God in Nigeria. On the
+                    International scene, the church is present in other African nations including C’ote D’Ivoire, Ghana,
+                    Zambia,
+                    Malawi, Zaire, Tanzania, Kenya, Uganda, Gambia, Cameroon, and South Africa. In Europe the church is
+                    spread
+                    in England, Germany, and France. In the United States there are parishes in Dallas, Tallahassee,
+                    Houston,
+                    New York, Washington, and Chicago and also in the Caribbean states of Haiti and Jamaica.
+
+                    Today, God is still doing marvelous deeds through the Redeemed Christian Church of God, worldwide. One
+                    of
+                    the well-known programs of the church is the Holy Ghost Service, an all night miracle service that holds
+                    on
+                    the first Friday of every month at the Redemption Camp at Km. 46, Lagos-Ibadan expressway. The average
+                    headcount of those who attend the Service is about 500,000. The Holy Ghost Service now holds in
+                    different
+                    parts of the world which includes the United Kingdom, India, USA, Canada, South Africa, Australia,
+                    Dubai,
+                    Ghana, Philippines and many more
+                </p>
+                <p className="underline">Our Vision</p>
+                <p>
+                    1) To make heaven.
+                    <br/>
+                    2) To take as many people with us.
+                    <br/>
+                    3) To have a member of RCCG in every family of all nations.
+                    <br/>
+                    4) To accomplish No. 1 above, holiness will be our lifestyle.
+                    <br/>
+                    5) To accomplish No. 2 and 3 above, we will plant churches within five minutes walking distance in every
+                    city and town of developing countries and within five minutes driving distance in every city and town of
+                    developed countries.
+                    <br/>
+                    6) We will pursue these objectives until every Nation in the world is reached for the Lord Jesus Christ
+                </p>
+
+                <p className="underline">THE BIBLE TEACHING</p>
+                <p>
+                    We believe that the entire Scripture, both Old and New Testament are written by the inspiration of the
+                    Holy Spirit – II Tim. 3:16-17. All the Christian teachings and the Christian attitude of the Children of
+                    God are such as are established in the Holy Bible – II Tim. 3:10-15; II Peter 1:21; John 17:17; Ps.
+                    119:105.
+                    <br />
+                    We believe that the Bible is the written and revealed Will of God. Man’s position to God, the way to
+                    obtain salvation, woe and destruction awaiting sinners that refuse to repent and everlasting joy that is
+                    kept for the born again souls. All the Bible teachings are holy, what the Bible reveals as the will of
+                    God are such that we should accept, and whatever God writes in the Bible and His Law are to remain
+                    unchangeable; for the Heavens and Earth may pass away but the Word of God stands forever. Deut. 4:22;
+                    Rev. 22:18, 19; Matt. 24:34-38.
+                </p>
+                <p className="underline">ABOUT GOD: Ps. 33:6-9</p>
+                <p>
+                    As revealed unto us by the Bible, we believe that there is only one God, Who is the Creator of both
+                    visible and invisible creatures -Gen. 1:1; Ps. 86:9-10; Is. 43:10-11; John 1:1-3. Only God will be in
+                    existence forever. Ez. 3:14; in God every creature receives life -John 5:26.
+                </p>
+                <p className="underline">ABOUT JESUS CHRIST</p>
+                <p>
+                    We believe that He is the Son of God; Who took away our sins, and the Savior of the world. We also
+                    believe that Jesus is God and was born by Mary the Virgin. He is God revealed in the flesh. Through Him
+                    all things were created. John 1:1-14; John 14:9; Matt. 1:18-25, Is 9:6; Is 53:5-6. We believe in His
+                    death on the Cross, and resurrection, by which He brought redemption – Is. 53:4-10; I Pet. 2:24; John
+                    10:11; Gal. 3:13, Matt. 20:28; I Cor. 15:3; Rom. 4:25; II Cor. 5:14; Heb 2:9; Heb 9:26.
+                </p>
+                <p className="underline">ABOUT THE HOLY SPIRIT</p>
+                <p>
+                    The Holy Spirit is the third Person in the TRINITY. He has the same power, the same glory with God the
+                    Father and God the Son – John 14:16-17; John 15:26; Matt. 3:16; Acts 13:24. He is one with Father and
+                    the Son who is to be worshiped and served. Three Persons that become one are the source of blessings to
+                    all the living creatures in Heaven and on earth – Matt. 28:19; II Cor. 13:14; I John 1:5-7. The Holy
+                    Spirit has a great work to perform. He Teaches, He Speaks to men, and bears witness in us – Acts 16:6-7;
+                    He performs the work of regeneration for man- John 3:5-6; He also performs the work of Sanctification in
+                    the born again souls until they are fully sanctified – John 16:8; Eph 1:17-19; II Thess. 2:13; I Cor.
+                    6:11. This same Holy Spirit endows believers with gifts they can use – I Cor 12:7; He empowers one in
+                    the Lord. Acts 1:8.
+                </p>
+                <p className="underline">ABOUT TRINITY</p>
+                <p>
+                    The egg is comprised of three parts: The yellow yolk, the whitish part, and the shell. Despite these
+                    three (3) substances, the egg is not three but one. Likewise, we believe that God the Father, God the
+                    Son, and God the Holy Ghost are one God, but made of three persons – hence they are ONE IN TRINITY –
+                    Gen. 1:16-29; Matt. 3:16-17; Matt. 18:19.
+                </p>
+                <p className="underline">ABOUT THE DEVIL</p>
+                <p>
+                    We believe that there is a devil, who seeks the down fall of every man. He brought sickness, sin and
+                    death into the world. Gen. 3:1-16. He seeks the destruction of those who exercise their faith in the
+                    Lord Jesus. Matt. 4:1-11; James 4:7; I Peter 5:8. The devil has several unclean spirits over whom he
+                    governs – Matt. 12:24. A time would soon come when he shall be thrown into the pit and chained for one
+                    thousand years- Rev. 20:7-9. After this, he will be put into the lake of fire where he will remain
+                    suffering together with his followers for ever and ever. Rev. 20:10
+                </p>
+                <p className="underline">ABOUT MAN</p>
+                <p>
+                    Things He created. Gen. 1:26. God formed man of the dust of the ground and breathed into his nostrils
+                    the breath of life, and man became a living soul – Gen 2:7. Therefore man is of three parts, namely
+                    body, soul and spirit. Man is God’s ambassador on earth to take control over all other creatures on His
+                    beh…
+                </p>
+            </div>
+        </div>
+    )
+;
 
 export default About;
